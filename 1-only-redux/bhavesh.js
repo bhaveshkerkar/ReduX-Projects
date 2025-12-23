baac665889454ccf4a9c80db3b38c883193046f4
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-23 20:43:22 +0530

    edit file

--- 1-only-redux/bhavesh.js
+++ 1-only-redux/bhavesh.js
@@ -1,8 +1,20 @@
 const { act } = require("react");
 const redux = require("redux");
 
-const reducer = (store, action) => {
-  return store;
+const INITIAL_VALUE = {
+  counter: 0,
+};
+const reducer = (store = INITIAL_VALUE, action) => {
+  return { counter: store.counter + 1 };
 };
 
-const store = redux.createStore(() => {});
+const store = redux.createStore(reducer);
+
+const subsciber = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+store.subscribe(subsciber);
+
+store.dispatch({ type: "INCREMENT" });

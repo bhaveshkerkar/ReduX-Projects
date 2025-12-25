804613a04ab84376fcaf2f2e3c3ddc2d22ebf05b
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-25 22:28:58 +0530

    final commit

--- 1-only-redux/bhavesh.js
+++ 1-only-redux/bhavesh.js
@@ -9,6 +9,8 @@ const reducer = (store = INITIAL_VALUE, action) => {
     newStore = { counter: store.counter + 1 };
   } else if (action.type === "DECREMENT") {
     newStore = { counter: store.counter - 1 };
+  } else if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
   }
   return newStore;
 };
@@ -23,3 +25,7 @@ const subsciber = () => {
 store.subscribe(subsciber);
 
 store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "ADDITION", payload: { number: 7 } });
+store.dispatch({ type: "DECREMENT" });

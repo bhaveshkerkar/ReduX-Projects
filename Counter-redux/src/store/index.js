104bbbfa7c99fc791dc8d11dc3237ed7fac45247
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-28 21:11:31 +0530

    edit store and add counters with incremanet and decreatement eith payload

---
+++ Counter-redux/src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+
+const INIRIAL_VALUE = {
+  counter: 5,
+};
+const counterReducer = (store = INIRIAL_VALUE, action) => {
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { counter: store.counter - 1 };
+  } else if (action.type === "ADD") {
+    return { counter: store.counter + Number(action.payload.num) };
+  } else if (action.type === "SUBTRACT") {
+    return { counter: store.counter - Number(action.payload.num) };
+  }
+  return store;
+};
+const counterStore = createStore(counterReducer);
+
+export default counterStore;

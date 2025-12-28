2a873c3ee4ab949e8eb54bd4c781de1744bab595
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-28 19:34:01 +0530

    edit Container

--- Counter-redux/src/App.jsx
+++ Counter-redux/src/App.jsx
@@ -3,35 +3,20 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
+import Controls from "./components/Controls";
 import Container from "./components/Container";
 
 function app() {
   return (
-    <>
-      <div className="px-4 py-5 my-5 text-center">
+    <center className="px-4 py-5 my-5 text-center">
+      <Container>
         <Header />
         <div className="col-lg-6 mx-auto">
           <DisplayCounter></DisplayCounter>
-          <Container></Container>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button
-              type="button"
-              className="btn btn-primary btn-lg px-4 gap-3"
-              fdprocessedid="uotl9"
-            >
-              Primary button
-            </button>
-            <button
-              type="button"
-              className="btn btn-outline-secondary btn-lg px-4"
-              fdprocessedid="422fpx"
-            >
-              Secondary
-            </button>
-          </div>
+          <Controls></Controls>
         </div>
-      </div>
-    </>
+      </Container>
+    </center>
   );
 }
 

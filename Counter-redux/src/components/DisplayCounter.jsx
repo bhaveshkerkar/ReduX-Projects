5152ccfbbb0296239b85ca4bf8dae65405808da2
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-29 01:52:31 +0530

    add 3rd project with use of toolkit

--- Counter-redux/src/components/DisplayCounter.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-
-const DisplayCounter = () => {
-  const counter = useSelector((store) => store.counter);
-
-  return <p className="lead mb-4">The current counter value is: {counter}</p>;
-};
-
-export default DisplayCounter;

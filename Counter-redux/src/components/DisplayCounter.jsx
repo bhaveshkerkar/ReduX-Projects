9ee9d4870b1cc2e6d69d73452525bf47687f7378
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-28 20:06:43 +0530

    edit file

--- Counter-redux/src/components/DisplayCounter.jsx
+++ Counter-redux/src/components/DisplayCounter.jsx
@@ -1,12 +1,9 @@
-const displayCounter = () => {
-  return (
-    <p className="lead mb-4">
-      Quickly design and customize responsive mobile-first sites with Bootstrap,
-      the world’s most popular front-end open source toolkit, featuring Sass
-      variables and mixins, responsive grid system, extensive prebuilt
-      components, and powerful JavaScript plugins.
-    </p>
-  );
+import { useSelector } from "react-redux";
+
+const DisplayCounter = () => {
+  const counter = useSelector((store) => store.counter);
+
+  return <p className="lead mb-4">The current counter value is: {counter}</p>;
 };
 
-export default displayCounter;
+export default DisplayCounter;

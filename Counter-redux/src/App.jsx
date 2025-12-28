b8f1a87227c97412aa0b027370768a76b15bd58c
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-28 21:39:50 +0530

    add toggle logic

--- Counter-redux/src/App.jsx
+++ Counter-redux/src/App.jsx
@@ -5,15 +5,18 @@ import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
 import Controls from "./components/Controls";
 import Container from "./components/Container";
+import { useSelector } from "react-redux";
+import PrivacyMessage from "./components/PrivacyMessage";
 
 function app() {
+  const privacy = useSelector((store) => store.privacy);
   return (
     <center className="px-4 py-5 my-5 text-center">
       <Container>
         <Header />
         <div className="col-lg-6 mx-auto">
-          <DisplayCounter></DisplayCounter>
-          <Controls></Controls>
+          {privacy ? <PrivacyMessage /> : <DisplayCounter />}
+          <Controls />
         </div>
       </Container>
     </center>

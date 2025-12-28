769cd3cce6ac177a89c2814797d8e0edc91847c9
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-29 01:30:00 +0530

    edit file

--- Counter-redux/src/store/index.js
+++ Counter-redux/src/store/index.js
@@ -10,10 +10,7 @@ const counterReducer = (store = INIRIAL_VALUE, action) => {
   } else if (action.type === "DECREMENT") {
     return { ...store, counter: store.counter - 1 };
   } else if (action.type === "ADD") {
-    return {
-      ...store,
-      counter: store.counter + Number(action.payload.num),
-    };
+    return { ...store, counter: store.counter + Number(action.payload.num) };
   } else if (action.type === "SUBTRACT") {
     return {
       ...store,

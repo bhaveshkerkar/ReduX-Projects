bc77497284e526382417178c6268af5475877f8d
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-28 20:05:49 +0530

    add actions increament and dicremanet

--- Counter-redux/src/components/Controls.jsx
+++ Counter-redux/src/components/Controls.jsx
@@ -1,11 +1,23 @@
 const Controls = () => {
+  const Controls = () => {
+    const dispatch = store.dispatch();
+
+    const handleIncrement = () => {
+      dispatch({ type: "INCREMENT" });
+    };
+
+    const handleDecrement = () => {
+      dispatch({ type: "DECREMENT" });
+    };
+  };
+
   return (
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" class="btn btn-primary">
+      <button type="button" className="btn btn-primary">
         +1
       </button>
 
-      <button type="button" class="btn btn-success">
+      <button type="button" className="btn btn-success">
         -1
       </button>
     </div>

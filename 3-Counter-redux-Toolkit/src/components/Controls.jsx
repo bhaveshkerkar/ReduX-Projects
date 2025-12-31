9020acd5508bd8999f1c9ff45137d7cb8caec9c1
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-31 23:59:11 +0530

    edit

--- 3-Counter-redux-Toolkit/src/components/Controls.jsx
+++ 3-Counter-redux-Toolkit/src/components/Controls.jsx
@@ -62,7 +62,7 @@ const Controls = () => {
           className="btn btn-warning"
           onClick={handlePrivacyToggle}
         >
-          Privacy Toggleeeeeeee
+          Privacy Toggleeeeeeeeeeee
         </button>
       </div>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">

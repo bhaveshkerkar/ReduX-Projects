c6d1117f2a83326d6d946cb744a0e5892f47052e
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2026-01-16 23:14:52 +0530

    edit after 10 days

--- 3-Counter-redux-Toolkit/src/components/Header.jsx
+++ 3-Counter-redux-Toolkit/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 className="display-5 fw-bold text-body-emphasis">Counter</h1>;
+  return <h1 className="display-5 fw-bold text-body-emphasis">counter</h1>;
 };
 export default Header;
